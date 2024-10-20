7f43814409f043d254ba57e4beedc1b662d9a936
redssy <redlemagic@hotmail.com>
2024-10-20 19:42:18 -0300

    adicionar reducx

--- src/containers/listaDeTarefas/index.tsx
+++ src/containers/listaDeTarefas/index.tsx
@@ -1,57 +1,65 @@
-import { useState } from 'react'
-import Tarefa, { PropsTarefa } from '../../componentes/filtroCard/tarefa'
+// import { useState } from 'react'
+import Tarefa from '../../componentes/filtroCard/tarefa' //{ PropsTarefa }
 import { ContainerTarefas } from './styles'
-import * as enums from '../../globais/enums'
+// import * as enums from '../../globais/enums'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
 /* eslint-disable react/no-unescaped-entities */
 const ListaTarefas = () => {
   // cria um vetor de objetos vazio do tipo 'PropsTarefa'
   //let itensTarefasFixa: PropsTarefa[] = []
   // sintaxe: let/const [nome, setNome] = useState<tipo/tipo[]>()/([])
-  const [itensTarefas, setItensTarefas] = useState<PropsTarefa[]>([])
+  //const [itensTarefas, setItensTarefas] = useState<PropsTarefa[]>([])
 
-  // validacao p/ adicionar itens de tarefas
-  if (itensTarefas.length === 0) {
-    // cria tarefas
-    const itens: PropsTarefa[] = [
-      {
-        descricao: 'Assistar aula 3 da EBAC',
-        prioridade: enums.EPrioridade.IMPORTANTE,
-        status: enums.EStatus.PENDENTE,
-        titulo: 'Estudar TypeScript'
-      },
-      {
-        descricao: 'Assistar aula 4 da EBAC',
-        prioridade: enums.EPrioridade.URGENTE,
-        status: enums.EStatus.CONCLUIDO,
-        titulo: 'Estudar React com Redux'
-      },
-      {
-        descricao: 'Assistar aula 10 da EBAC',
-        prioridade: enums.EPrioridade.NORMAL,
-        status: enums.EStatus.PENDENTE,
-        titulo: 'Estudar JavaScript'
-      }
-    ]
-    // preenche vetor vazio
-    setItensTarefas(itens)
-  }
+  // // validacao p/ adicionar itens de tarefas
+  // if (itensTarefas.length === 0) {
+  //   // cria tarefas
+  //   const itens: PropsTarefa[] = [
+  //     {
+  //       descricao: 'Assistar aula 3 da EBAC',
+  //       prioridade: enums.EPrioridade.IMPORTANTE,
+  //       status: enums.EStatus.PENDENTE,
+  //       titulo: 'Estudar TypeScript'
+  //     },
+  //     {
+  //       descricao: 'Assistar aula 4 da EBAC',
+  //       prioridade: enums.EPrioridade.URGENTE,
+  //       status: enums.EStatus.CONCLUIDO,
+  //       titulo: 'Estudar React com Redux'
+  //     },
+  //     {
+  //       descricao: 'Assistar aula 10 da EBAC',
+  //       prioridade: enums.EPrioridade.NORMAL,
+  //       status: enums.EStatus.PENDENTE,
+  //       titulo: 'Estudar JavaScript'
+  //     }
+  //   ]
+  //   // preenche vetor vazio
+  //   setItensTarefas(itens)
+  // }
+
+  // utilizando o 'seletor' do 'redux'
+  // buscando as tarefas sem desestruturar
+  //const tarefas = useSelector((estado: RootReducer) => estado.tarefas)
+  // busca as tarefas desestruturando
+  const { tarefas } = useSelector((estado: RootReducer) => estado)
 
   return (
     <ContainerTarefas>
-      {itensTarefas.length > 0 && (
+      {tarefas.length > 0 && (
         <>
           <p>2 tarefas marcadas como: "categoria" e "termo"</p>
           <ul>
             {/* renderiza os itens em loop */}
-            {itensTarefas.map((itemTarefa, i) => (
+            {tarefas.map((itemTarefa, i) => (
               // cria um item passando um ID como 'chave'
               <li key={i + ' - ' + itemTarefa.titulo}>
                 {/* tarefa atual */}
                 <Tarefa
                   descricao={itemTarefa.descricao}
                   prioridade={itemTarefa.prioridade}
-                  status={itemTarefa.status}
+                  status={itemTarefa.estado}
                   titulo={itemTarefa.titulo}
                 ></Tarefa>
               </li>
